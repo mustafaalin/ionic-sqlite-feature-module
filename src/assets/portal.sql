b4840d2b0535dee66ef1c337138459ed80d6614a
aysenurkocak <38115567+aysenurkocak@users.noreply.github.com>
2020-01-12 22:50:24 +0300

    grafik için ilaçlar tablosu

--- src/assets/portal.sql
+++ src/assets/portal.sql
@@ -13,3 +13,10 @@ INSERT INTO note (title, note, icon) VALUES ('American Football', 'This item Ame
 INSERT INTO note (title, note, icon) VALUES ('Boat', 'This item Boat', 'boat');
 INSERT INTO note (title, note, icon) VALUES ('Bluetooth', 'This item Bluetooth', 'bluetooth');
 INSERT INTO note (title, note, icon) VALUES ('Build', 'This item Build', 'build');
+DROP TABLE IF EXISTS ilaclar;
+CREATE TABLE IF NOT EXISTS berberler (id INTEGER PRIMARY KEY AUTOINCREMENT,adi TEXT, ilacSayisi INT);
+INSERT INTO ilaclar (adi, ilacSayisi) VALUES ('Majazig',20);
+INSERT INTO ilaclar (adi, ilacSayisi) VALUES ('Asprin',15);
+INSERT INTO ilaclar (adi, ilacSayisi) VALUES ('Parol',10);
+INSERT INTO ilaclar(kadi, ilacSayisi) VALUES ('Vitamin',10);
+
